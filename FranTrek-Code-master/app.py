5cc94a719d98d67a50012ec0d457a2c3377026f4
Francianeny <nirinasoabozy@yahoo.fr>
2024-10-17 10:29:27 +0200

    create files contact.html and about.html

--- FranTrek-Code-master/app.py
+++ FranTrek-Code-master/app.py
@@ -1,4 +1,15 @@
+from flask import redirect, render_template, request, url_for
 from FranTrek import app
 
 if __name__ == "__main__":
-    app.run(debug=True,port=4000)
+    app.run(debug=True,port=4001)
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    if request.method == 'POST':
+        info = request.form.get('about-info')
+        # Traite les informations reçues
+        print(info)  # Ici, tu peux faire ce que tu veux avec les données
+        return redirect(url_for('about'))
+    return render_template('about.html')
