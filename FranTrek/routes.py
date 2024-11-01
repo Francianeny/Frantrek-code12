67c32a4acc02db2b8a6e3ee30ad7a0a4456709f9
Francianeny <nirinasoabozy@yahoo.fr>
2024-11-01 14:06:29 +0100

    start from zero

---
+++ FranTrek/routes.py
@@ -0,0 +1,26 @@
+# import secrets
+# from PIL import Image
+# import os
+# from FranTrek.models import User, Lesson, Course
+# from flask_ckeditor import upload_success, upload_fail
+# from flask_mail import Message 
+# from flask import render_template, url_for, flash, redirect, request, session, abort, send_from_directory
+# from FranTrek.forms import (
+#     NewCourseForm,
+#     NewLessonForm,
+#     RegistrationForm,
+#     LoginForm,
+#     UpdateProfileForm,
+#     LessonUpdateForm,
+#     RequestResetForm,
+#     ResetPasswordForm,
+# )
+# from FranTrek import app, bcrypt, db, mail
+# from flask_modals import render_template_modal
+# from flask_login import (
+#     login_required,
+#     login_user,
+#     current_user,
+#     logout_user,
+#     login_required,
+# )
